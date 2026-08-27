69f4790df64305ff5e2b519c045f7de1eaad083d
Eli Sheets <eli.sheets@gmail.com>
2026-08-27 12:21:09 -0400

    ideablock-commit 3.1.1
    sha256 verified against the published tarball.

--- Formula/ideablock-commit.rb
+++ Formula/ideablock-commit.rb
@@ -1,8 +1,8 @@
 class IdeablockCommit < Formula
   desc "Anchor every git commit on the Bitcoin blockchain"
   homepage "https://app.ideablock.com"
-  url "https://registry.npmjs.org/ideablock-commit/-/ideablock-commit-3.1.0.tgz"
-  sha256 "a53ca5e79490067971f243c33c41465251489ce24691a628f277b8c735846002"
+  url "https://registry.npmjs.org/ideablock-commit/-/ideablock-commit-3.1.1.tgz"
+  sha256 "8b66437a3af9cbda14febcd9e4b607b21da04edbd346d4d42db317298890b605"
   license "Apache-2.0"
 
   depends_on "node"
